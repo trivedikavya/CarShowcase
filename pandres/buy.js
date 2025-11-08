dc275c4f5fbfaa74e23444cac729aa9c5fc4217e
KAVYA TRIVEDI ⚡ <149308388+trivedikavya@users.noreply.github.com>
2025-11-08 19:09:57 +0530

    Add files via upload

---
+++ pandres/buy.js
@@ -0,0 +1,3 @@
+function buyy(){
+    alert("Here your bank notifys you that \" bhai aukat mein dekh ke kharida kar\" if you  still have to pay so pay to kavya trivedi \(no guarantee that product Will reach to you yeah something will reach to you that the notification of debit from us \)")
+}
